82d695e5d4a7ebcc14f9b5ed6c41b635e758dadf
ichisemkk <ichisemkk@gmail.com>
2026-08-10 22:31:24 +0900

    移動機CRUD APIを実装

--- backend/src/main/java/backend/controller/MobileController.java
+++ backend/src/main/java/backend/controller/MobileController.java
@@ -3,10 +3,18 @@
 package backend.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
 import backend.entity.Mobile;
 import backend.service.MobileService;
@@ -25,4 +33,53 @@ public class MobileController {
     public List<Mobile> findAll() {
         return mobileService.findAll();
     }
+
+// IDで1件取得する処理
+    @GetMapping("/{id}")
+    public Mobile findById(@PathVariable Long id) {
+        return mobileService.findById(id);
+    }
+
+//登録API
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Map<String, Object> create(@RequestBody Mobile mobile) {
+
+        Mobile savedMobile = mobileService.save(mobile);
+
+        return Map.of(
+                "result", "success",
+                "message", "登録しました",
+                "mobile", savedMobile
+        );
+    }
+
+//更新API
+    @PutMapping("/{id}")
+    public Map<String, Object> update(
+            @PathVariable Long id,
+            @RequestBody Mobile mobile) {
+
+        Mobile updatedMobile = mobileService.update(id, mobile);
+
+        return Map.of(
+                "result", "success",
+                "message", "更新しました",
+                "mobile", updatedMobile
+        );
+    }
+
+//削除API
+   @DeleteMapping("/{id}")
+   public Map<String, String> delete(@PathVariable Long id) {
+
+       mobileService.delete(id);
+
+       return Map.of(
+               "result", "success",
+               "message", "削除しました"
+       );
+}
+
+
 }
--- backend/src/main/java/backend/service/MobileService.java
+++ backend/src/main/java/backend/service/MobileService.java
@@ -2,13 +2,15 @@ package backend.service;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import backend.entity.Mobile;
 import backend.repository.MobileRepository;
 
 
-// まずは一覧取得だけできる最小構成
+// 一覧取得だけできる最小構成
 @Service
 public class MobileService {
 
@@ -21,4 +23,54 @@ public class MobileService {
     public List<Mobile> findAll() {
         return mobileRepository.findAll();
     }
+
+// IDで1件取得する処理&404追加
+    public Mobile findById(Long id) {
+       return mobileRepository.findById(id)
+            .orElseThrow(() -> new ResponseStatusException(
+                    HttpStatus.NOT_FOUND,
+                    "対象の移動機が見つかりません"
+            ));
+    }
+//登録API
+    public Mobile save(Mobile mobile) {
+        return mobileRepository.save(mobile);
+    }
+
+//更新API
+    public Mobile update(Long id, Mobile updatedMobile) {
+
+        Mobile mobile = mobileRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND,
+                        "対象の移動機が見つかりません"
+                ));
+
+        mobile.setMobileName(updatedMobile.getMobileName());
+        mobile.setMacAddress(updatedMobile.getMacAddress());
+        mobile.setSerialNumber(updatedMobile.getSerialNumber());
+        mobile.setMobileColor(updatedMobile.getMobileColor());
+        mobile.setDriverVersion(updatedMobile.getDriverVersion());
+        mobile.setLenderReturnDestination(updatedMobile.getLenderReturnDestination());
+        mobile.setLenderContactPerson(updatedMobile.getLenderContactPerson());
+        mobile.setLenderBorrowedDate(updatedMobile.getLenderBorrowedDate());
+        mobile.setLenderReturnedDate(updatedMobile.getLenderReturnedDate());
+        mobile.setLenderReturnDueDate(updatedMobile.getLenderReturnDueDate());
+        mobile.setStatus(updatedMobile.getStatus());
+        mobile.setRemarks(updatedMobile.getRemarks());
+
+        return mobileRepository.save(mobile);
+    }
+
+//削除API
+    public void delete(Long id) {
+
+        Mobile mobile = mobileRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND,
+                        "対象の移動機が見つかりません"
+                ));
+        mobileRepository.delete(mobile);
+    }
+
 }
